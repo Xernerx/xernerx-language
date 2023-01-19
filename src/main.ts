11a3fd52bd015538fb2285075737cb200d47d4e9
TheDummi <dummidevelopment@gmail.com>
2023-01-19 22:42:51 +0100

    Version Patch

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,46 @@
-class XernerxLanguage {
-    name: string;
-    client: Client;
-
-    constructor(client: Client) {
-        this.name = "XernerxLanguage";
-
-        this.client = client;
-    }
-}
-
+import XernerxExtensionBuilder from 'xernerx-extension-builder';
+import i18next, { InitOptions } from 'i18next';
+import * as path from 'node:path';
+import * as fs from 'node:fs';
 interface Client {
-
+	languages: Record<string, string>;
+	translate: Function;
 }
 
-export { XernerxLanguage }
+interface Options {
+	directory: string;
+}
+export default class XernerxLanguage extends XernerxExtensionBuilder {
+	client: Client;
+	options: InitOptions | Options;
+
+	constructor(client: Client, options: InitOptions | Options) {
+		super('XernerxLanguage');
+
+		this.client = client;
+
+		this.client.languages = {};
+
+		this.options = options as Options;
+
+		i18next
+			.init({
+				lng: 'en',
+			})
+			.then((e) => console.log(e));
+
+		this.#addBundles();
+	}
+
+	async #addBundles() {
+		const files = fs.readdirSync(path.resolve((this.options as Options).directory));
+
+		for (const file of files) {
+			const language = await import('file://' + path.resolve((this.options as Options).directory) + '/' + file, { assert: { type: 'json' } });
+
+			this.client.languages[file.replace('.json', '')] = file.replace('.json', '');
+			//   ^?
+			i18next;
+		}
+	}
+}
